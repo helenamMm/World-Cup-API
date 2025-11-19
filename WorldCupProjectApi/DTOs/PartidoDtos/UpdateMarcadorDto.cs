dbbf80a80506d481f6d81702fcf92035b31e7302
helenamMm <helenamm.001@gmail.com>
2025-11-18 18:40:22 -0600

    Aveces me cansa git

--- WorldCupProjectApi/DTOs/PartidoDtos/UpdateMarcadorDto.cs
+++ WorldCupProjectApi/DTOs/PartidoDtos/UpdateMarcadorDto.cs
@@ -3,7 +3,7 @@ namespace WorldCupProjectApi.DTOs.PartidoDtos;
 public class UpdateMarcadorDto
 {
     public int GolesEquipoA { get; set; } = 0;
-    public int GolesEquipoB { get; set; } = 0;git
+    public int GolesEquipoB { get; set; } = 0;
     
     public string Jugador { get; set; }
 }
